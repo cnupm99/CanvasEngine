206cb6ea730bbe18e65bba114ece10cac1f6115a
cnupm99 <maksimvinogradov@gmail.com>
2017-05-02 10:18:38 +0300

    ProgressBar первая версия

--- utils/ProgressBar/ProgressBar.coffee
+++ utils/ProgressBar/ProgressBar.coffee
@@ -22,10 +22,9 @@ define () ->
 			colors = options.colors or {}
 			@_colors = {
 
-				backgroundColor: colors.backgroundColor or "#CCC"
-				backgroundShadowColor: colors.backgroundShadowColor or "#000"
+				backgroundColor: colors.backgroundColor or ["#C3BD73", "#DCD9A2"]
+				backgroundShadowColor: colors.backgroundShadowColor or "#FFF"
 
-				progress5: colors.progress5 or ["#f63a0f", "#E72B00"]
 				progress25: colors.progress25 or ["#f27011", "#E36102"]
 				progress50: colors.progress50 or ["#f2b01e", "#E3A10F"]
 				progress75: colors.progress75 or ["#f2d31b", "#E3C40C"]
@@ -34,13 +33,16 @@ define () ->
 				progressShadowColor: colors.progressShadowColor or "#000"
 
 				caption: colors.caption or "#B22222"
-				captionStroke: colors.captionStroke or "#000"
+				captionStroke: colors.captionStroke or "#A11111"
 
 			}
 
 			@_position = options.position or [0, 0]
 			@_sizes = options.sizes or [300, 50]
 			@_padding = options.padding or 3
+
+			@_rounded = if options.rounded? then options.rounded else true
+			@_radius = options.radius or 5
 
 			@_showCaption = if options.showCaption? then options.showCaption else false
 			@_caption = options.caption or "Progress: "
@@ -88,35 +90,69 @@ define () ->
 			document.body.removeChild span
 			return height
 
+		# рисуем пряоугольник со скругленными углами
+		_drawRoundedRect: (context, x, y, width, height, radius) ->
+
+			# предварительные вычисления
+			pi = Math.PI
+			halfpi = pi / 2
+			x1 = x + radius
+			x2 = x + width - radius
+			y1 = y + radius
+			y2 = y + height - radius
+			# рисуем
+			context.moveTo x1, y
+			context.lineTo x2, y
+			context.arc x2, y1, radius, -halfpi, 0
+			context.lineTo x + width, y2
+			context.arc x2, y2, radius, 0, halfpi
+			context.lineTo x1, y + height
+			context.arc x1, y2, radius, halfpi, pi
+			context.lineTo x, y1
+			context.arc x1, y1, radius, pi, 3 * halfpi
+
+		_setGradient: (context, color1, color2) ->
+
+			gradient = context.createLinearGradient @_padding, @_padding, @_padding, @_sizes[1] - @_padding * 2
+			gradient.addColorStop 0, color2
+			gradient.addColorStop 0.5, color1
+			gradient.addColorStop 1, color2
+			context.fillStyle = gradient
+
 		animate: (context) ->
 
 			context.save()
 
 			context.translate @_position[0], @_position[1]
 
-			context.fillStyle = @_colors.backgroundColor
 			context.shadowColor = @_colors.backgroundShadowColor
-			context.shadowBlur = 2
+			context.shadowBlur = 3
 			context.shadowOffsetX = 0
 			context.shadowOffsetY = 0
-			context.fillRect 0, 0, @_sizes[0], @_sizes[1]
+			@_setGradient context, @_colors.backgroundColor[0], @_colors.backgroundColor[1]
 
-			if @_progress <= 5 then color = @_colors.progress5
-			else if @_progress <= 25 then color = @_colors.progress25
+			context.beginPath()
+			if @_rounded then @_drawRoundedRect context, 0, 0, @_sizes[0], @_sizes[1], @_radius else context.rect 0, 0, @_sizes[0], @_sizes[1]
+			context.strokeStyle = "#000"
+			context.stroke()
+			context.fill()
+
+			if @_progress <= 25 then color = @_colors.progress25
 			else if @_progress <= 50 then color = @_colors.progress50
 			else if @_progress <= 75 then color = @_colors.progress75
 			else color = @_colors.progress100
 
 			size = Math.floor((@_sizes[0] - @_padding * 2) * @_value / @_maxValue)
 
-			gradient = context.createLinearGradient @_padding, @_padding, @_padding, @_sizes[1] - @_padding * 2
-			gradient.addColorStop 0, color[0]
-			gradient.addColorStop 1, color[1]
-			context.fillStyle = gradient
+			@_setGradient context, color[0], color[1]
 
 			context.shadowColor = @_colors.progressShadowColor
 
-			context.fillRect @_padding, @_padding, size, @_sizes[1] - @_padding * 2
+			context.beginPath()
+			if @_rounded then @_drawRoundedRect context, @_padding, @_padding, size, @_sizes[1] - @_padding * 2, @_radius else context.rect @_padding, @_padding, size, @_sizes[1] - @_padding * 2
+			context.fill()
+
+			context.shadowColor = "rgba(0, 0, 0, 0)"
 
 			text = ""
 			text += @_caption if @_showCaption
@@ -132,6 +168,13 @@ define () ->
 				width = context.measureText(text).width
 				context.textBaseline = "top"
 				context.fillText text, (@_sizes[0] - width) / 2, (@_sizes[1] - @_fontHeight) / 2
+				
+				if @_strokeCaption
+
+					context.lineWidth = 1
+					context.strokeStyle = @_colors.captionStroke
+					context.strokeText text, (@_sizes[0] - width) / 2, (@_sizes[1] - @_fontHeight) / 2
+
 
 			@needAnimation = false
 
--- utils/ProgressBar/ProgressBar.js
+++ utils/ProgressBar/ProgressBar.js
@@ -22,20 +22,21 @@
         }
         colors = options.colors || {};
         this._colors = {
-          backgroundColor: colors.backgroundColor || "#CCC",
-          backgroundShadowColor: colors.backgroundShadowColor || "#000",
-          progress5: colors.progress5 || ["#f63a0f", "#E72B00"],
+          backgroundColor: colors.backgroundColor || ["#C3BD73", "#DCD9A2"],
+          backgroundShadowColor: colors.backgroundShadowColor || "#FFF",
           progress25: colors.progress25 || ["#f27011", "#E36102"],
           progress50: colors.progress50 || ["#f2b01e", "#E3A10F"],
           progress75: colors.progress75 || ["#f2d31b", "#E3C40C"],
           progress100: colors.progress100 || ["#86e01e", "#67C000"],
           progressShadowColor: colors.progressShadowColor || "#000",
           caption: colors.caption || "#B22222",
-          captionStroke: colors.captionStroke || "#000"
+          captionStroke: colors.captionStroke || "#A11111"
         };
         this._position = options.position || [0, 0];
         this._sizes = options.sizes || [300, 50];
         this._padding = options.padding || 3;
+        this._rounded = options.rounded != null ? options.rounded : true;
+        this._radius = options.radius || 5;
         this._showCaption = options.showCaption != null ? options.showCaption : false;
         this._caption = options.caption || "Progress: ";
         this._font = options.font || "24px Arial";
@@ -87,19 +88,53 @@
         return height;
       };
 
+      ProgressBar.prototype._drawRoundedRect = function(context, x, y, width, height, radius) {
+        var halfpi, pi, x1, x2, y1, y2;
+        pi = Math.PI;
+        halfpi = pi / 2;
+        x1 = x + radius;
+        x2 = x + width - radius;
+        y1 = y + radius;
+        y2 = y + height - radius;
+        context.moveTo(x1, y);
+        context.lineTo(x2, y);
+        context.arc(x2, y1, radius, -halfpi, 0);
+        context.lineTo(x + width, y2);
+        context.arc(x2, y2, radius, 0, halfpi);
+        context.lineTo(x1, y + height);
+        context.arc(x1, y2, radius, halfpi, pi);
+        context.lineTo(x, y1);
+        return context.arc(x1, y1, radius, pi, 3 * halfpi);
+      };
+
+      ProgressBar.prototype._setGradient = function(context, color1, color2) {
+        var gradient;
+        gradient = context.createLinearGradient(this._padding, this._padding, this._padding, this._sizes[1] - this._padding * 2);
+        gradient.addColorStop(0, color2);
+        gradient.addColorStop(0.5, color1);
+        gradient.addColorStop(1, color2);
+        return context.fillStyle = gradient;
+      };
+
       ProgressBar.prototype.animate = function(context) {
-        var color, gradient, size, text, width;
+        var color, size, text, width;
         context.save();
         context.translate(this._position[0], this._position[1]);
-        context.fillStyle = this._colors.backgroundColor;
         context.shadowColor = this._colors.backgroundShadowColor;
-        context.shadowBlur = 2;
+        context.shadowBlur = 3;
         context.shadowOffsetX = 0;
         context.shadowOffsetY = 0;
-        context.fillRect(0, 0, this._sizes[0], this._sizes[1]);
-        if (this._progress <= 5) {
-          color = this._colors.progress5;
-        } else if (this._progress <= 25) {
+        this._setGradient(context, this._colors.backgroundColor[0], this._colors.backgroundColor[1]);
+        context.beginPath();
+        if (this._rounded) {
+          this._drawRoundedRect(context, 0, 0, this._sizes[0], this._sizes[1], this._radius);
+        } else {
+          context.rect(0, 0, this._sizes[0], this._sizes[1]);
+        }
+        context.strokeStyle = "#000";
+        context.stroke();
+        context.fill();
+        if (this._progress <= 25) {
           color = this._colors.progress25;
         } else if (this._progress <= 50) {
           color = this._colors.progress50;
@@ -109,12 +144,16 @@
           color = this._colors.progress100;
         }
         size = Math.floor((this._sizes[0] - this._padding * 2) * this._value / this._maxValue);
-        gradient = context.createLinearGradient(this._padding, this._padding, this._padding, this._sizes[1] - this._padding * 2);
-        gradient.addColorStop(0, color[0]);
-        gradient.addColorStop(1, color[1]);
-        context.fillStyle = gradient;
+        this._setGradient(context, color[0], color[1]);
         context.shadowColor = this._colors.progressShadowColor;
-        context.fillRect(this._padding, this._padding, size, this._sizes[1] - this._padding * 2);
+        context.beginPath();
+        if (this._rounded) {
+          this._drawRoundedRect(context, this._padding, this._padding, size, this._sizes[1] - this._padding * 2, this._radius);
+        } else {
+          context.rect(this._padding, this._padding, size, this._sizes[1] - this._padding * 2);
+        }
+        context.fill();
+        context.shadowColor = "rgba(0, 0, 0, 0)";
         text = "";
         if (this._showCaption) {
           text += this._caption;
@@ -128,6 +167,11 @@
           width = context.measureText(text).width;
           context.textBaseline = "top";
           context.fillText(text, (this._sizes[0] - width) / 2, (this._sizes[1] - this._fontHeight) / 2);
+          if (this._strokeCaption) {
+            context.lineWidth = 1;
+            context.strokeStyle = this._colors.captionStroke;
+            context.strokeText(text, (this._sizes[0] - width) / 2, (this._sizes[1] - this._fontHeight) / 2);
+          }
         }
         this.needAnimation = false;
         return context.restore();
